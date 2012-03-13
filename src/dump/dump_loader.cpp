652bbe753fc06a7ea5704231069e323dbe80dd24
Jarosław Gomułka <jaroslaw.gomulka@gmail.com>
2012-03-14 00:42:11 +0100

    Fixing previous commit \(I didn\;t remove few debug outputs\)

--- src/dump/dump_loader.cpp
+++ src/dump/dump_loader.cpp
@@ -14,32 +14,27 @@ DumpLoader::~DumpLoader()
 
 
 std::string DumpLoader::get_line() {
-  std::cerr << "GET LINE\n";
   std::string line;
   std::getline(*input, line);
   return line;
 }
 
 bool DumpLoader::is_eof() {
-  std::cerr << "IS EOF " << input->peek() << "\n";
   return !input->good() || input->eof() || input->peek() == '\0';
 }
 
 int DumpLoader::get_next_int() {
-  std::cerr << "NEXT INT\n";
   int res;
   *input >> res;
   return res;
 }
 
 char DumpLoader::get_next_char() {
-  std::cerr << "NEXT CHAR\n";
   input->read(temp_buffor, 1);
   return temp_buffor[0];
 }
 
 std::string DumpLoader::get_next_chars(const int size) {
-  std::cerr << "NEXT CHARS\n";
   char ch;
   std::string res;
 
