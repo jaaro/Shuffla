2c94ec7caaa5e2bd5c1c793ba9ff15bfd1450d50
Jaroslaw Gomulka <jaroslaw.gomulka@gmail.com>
2012-04-14 21:22:52 +0200

    I forgot to add two files

---
+++ src/engine/query/delete/query_delete.cpp
@@ -0,0 +1,49 @@
+#include <boost/algorithm/string/predicate.hpp>
+
+#include "query_delete.hpp"
+
+const std::string QueryDelete::HTTP_PREFIX = "/delete/";
+
+QueryDelete::QueryDelete()
+{
+    //ctor
+}
+
+QueryDelete::~QueryDelete()
+{
+    //dtor
+}
+
+bool QueryDelete::set(const std::string& request)
+{
+    Query::set(request);
+    return parsed_query.set(request.substr(HTTP_PREFIX.size()));
+}
+
+Query* QueryDelete::parse(const std::string& request) const
+{
+    if (boost::starts_with(request, HTTP_PREFIX)) {
+        QueryDelete* result = new QueryDelete();
+        if (result->set(request)) {
+            return result;
+        }
+        delete result;
+    }
+
+    return NULL;
+}
+
+bool QueryDelete::is_modyfing_data() const
+{
+    return true;
+}
+
+std::string QueryDelete::get_table_name() const
+{
+    return parsed_query.get_table_name();
+}
+
+DataWithoutTyping QueryDelete::get_parsed_query() const
+{
+    return parsed_query;
+}
---
+++ src/engine/query/delete/query_delete.hpp
@@ -0,0 +1,25 @@
+#ifndef QUERYDELETE_H
+#define QUERYDELETE_H
+
+#include "../query.hpp"
+#include "../data_without_typing.hpp"
+
+class QueryDelete : public Query
+{
+public:
+    static const std::string HTTP_PREFIX;
+
+    QueryDelete();
+    virtual ~QueryDelete();
+
+    virtual bool set(const std::string& request);
+    virtual Query* parse(const std::string& request) const ;
+    virtual bool is_modyfing_data() const;
+    DataWithoutTyping get_parsed_query() const;
+    std::string get_table_name() const;
+protected:
+private:
+    DataWithoutTyping parsed_query;
+};
+
+#endif // QUERYDELETE_H
